2b0a7c71aa5ee2e9c929d1f97e9424fd5dbaee1f
Miguel Alvarez <miguelazpeitia@yahoo.com>
2012-12-14 21:57:35 -0600

    Cambio3
    Cambio en el salo de pagina

--- zeptoJs/actions.js
+++ zeptoJs/actions.js
@@ -51,7 +51,7 @@ function mostrarLocal() {
 	$('#local').tap(function(){
 		var key = window.localStorage.key(0);
 		var name = window.localStorage.getItem("llave");
-		navigator.notification.confirm('Llave: ' +key+'/nNombre: ',function(btn){
+		navigator.notification.confirm('Llave: ' +key+'\nNombre: ',function(btn){
 			if(btn==1) {
 				window.localStorage.clear();
 			}
